8ae3ac0dd081049e7a0c72f74f19d65c1ca84bb3
Aon1840 <aonattapon1840.ap@gmail.com>
2019-01-25 01:25:29 +0700

    Create index.html

---
+++ HomePage/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+# Create your views here.
+
+def indexPage(request):
+    return render(request, 'index2.html')
